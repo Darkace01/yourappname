e9a383bc630d17d1bb9cabe70500fd4a24edf1ab
Kazeem Quadri <33027659+Darkace01@users.noreply.github.com>
2020-08-21 14:38:01 +0100

    Rendering List using map metthod

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,12 +2,18 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
+    tags: ["tag1", "tag2", "tag3"],
   };
   render() {
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button className="btn btn-default btn-sm">Increment</button>
+        <ul>
+          {this.state.tags.map((tag) => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
       </div>
     );
   }
